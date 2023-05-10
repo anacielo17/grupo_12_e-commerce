9ebe4bdee702f83111fab957c87ee6dba2f23fd5
Rascasse01 <k12williams@hotmail.com>
2023-05-10 19:21:35 -0300

    Catalogo agregado

--- agro-mapache/app.js
+++ agro-mapache/app.js
@@ -9,6 +9,10 @@ app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "./views/index.html"))
 });
 
+app.get("/Catalogo", function(req,res){
+    res.sendFile(path.join(__dirname, "./views/catalogo.html"))
+});
+
 app.listen(2000, () =>{
     console.log("Servidor corriendo en el puerto 2000");
 });
