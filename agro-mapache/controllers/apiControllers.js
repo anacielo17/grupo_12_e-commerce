3277248a29bb88c18dd27747dd507760d2095843
anacielo17 <anacieloseidel@gmail.com>
2023-08-30 16:51:17 -0300

    Cambios controller de api para  acceder a la imagen

--- agro-mapache/controllers/apiControllers.js
+++ agro-mapache/controllers/apiControllers.js
@@ -58,29 +58,22 @@ module.exports = {
     productDetail: async (req, res) => {
         const product = await db.Product.findByPk(req.params.product_id, {
             include: [{ association: 'category', attributes: ["category_name"] }, { association: 'brand', attributes: ["brand_name"] }],
-            attributes: ['product_id', 'name', 'description'],
+            attributes: ['product_id', 'name', 'description', 'image'],
             raw: true
         })
 
         if (product) {
-            console.log(product);
-
-            const productImageURL = `/api/user/${product.product_id}/image`;
-
-            product.productImage = productImageURL; 
-
-            res.json(product);
-        } else {
-            return res.status(404).json({ error: `El producto no existe` });
+            const ecommerce = "http://localhost:2000/"
+            if (product) {
+            
+                res.json({product, 
+                        image : `${ecommerce}img/products/${product.image}`});
+            } else {
+                return res.status(404).json({ error: `El producto no existe` });
+            }
         }
     },
-    productImage: async (req, res) => {
-        const product = await db.Product.findByPk(req.params.product_id, {
-            attributes: ["image"],
-            raw: true
-        })
-        res.json(product)
-    },
+
 
 
 
@@ -94,6 +87,7 @@ module.exports = {
             raw: true,
 
         })
+        
         const userCollection = users.map(customer => ({
             id: customer.customer_id,
             name: customer.customer_name,
@@ -112,105 +106,19 @@ module.exports = {
             attributes: ['customer_id', 'customer_name', 'customer_lastName', "customer_phone", "customer_email", "customer_gender", "image", "customer_birthdate", "customer_country"],
             raw: true
         })
-
+        const ecommerce = "http://localhost:2000/"
         if (customer) {
-            console.log(customer);
-
-            const profileImageURL = `/api/user/${customer.customer_id}/image`;
-
-            customer.profileImage = profileImageURL; // Cambio 'user' a 'customer'
-
-            res.json(customer);
+        
+            res.json({customer, 
+                    image : `${ecommerce}img/user/${customer.image}`});
         } else {
             return res.status(404).json({ error: `El cliente no existe` });
         }
     },
 
-    userImage: async (req, res) => {
-        const customer = await db.Customer.findByPk(req.params.customer_id);
-    
-        if (customer && customer.image) {
-            // Si el cliente y la propiedad 'image' existen, redirige a la URL de la imagen
-            res.redirect("/api/user/:customer_id"+customer.image);
-        } else {
-            return res.status(404).json({ error: 'Imagen no encontrada' });
-        } 
-    } 
     }
     
     
     
     
-    
-
-// No salio :( 
-/* usersList: async (req, res) => {
-    const users = await db.Customer.findAll({
-        attributes: ['customer_id', 'customer_name', 'customer_email'],
-        raw: true,
-    });
-
-    const addDetailRouteToObjects = (objList, route, field) => {
-        const url = envs.APP_URL + ':' + envs.APP_PORT;
-        return objList.map(item => {
-            item.detail = url + route + item[field];
-            return item;
-        });
-    };
-
-    const usersWithDetail = addDetailRouteToObjects(users, '/user/', 'customer_id');
-
-    console.log(usersWithDetail);
-    res.json({
-        users: usersWithDetail,
-        total: users.length, // Cambio aquí
-    });
-},
-
-userDetail: async (req, res) => {
-    const findUser = req.params.customer_name;
-    let customer = await db.Customer.findOne({
-        where: { customer_name: findUser },
-    });
-
-    if (customer) {
-        const urlBase = envs.APP_URL + ':' + envs.APP_PORT;
-        customer.dataValues.imageURL =
-            urlBase + '/images/userProfile/' + customer.image; // Cambio aquí
-        delete customer.dataValues.customer_id; // Cambio aquí
-        delete customer.dataValues.password;
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        return res.json(customer);
-    } else {
-        return res
-            .status(404)
-            .json({ user: `El usuario ${findUser} no existe` });
-    }
-}        
- */
-
-/* const addObjOfImage = (productDetail)=>{
- let product = productDetail;
- let arrayOfImages = JSON.parse(product.dataValues.image);
- let objImage= {};
- const url= envs.APP_URL+':'+ envs.APP_PORT;
- arrayOfImages.forEach((image,i) =>{
-     objImage[i+1]= url +'/images/productos/'+image;
- })
- delete(product.dataValues.image)
- delete(product.dataValues.category_id)
- delete(product.dataValues.brand_code)
- product.dataValues.image= objImage;
- return product;
-}
-const addDetailRouteToObjects = (objList, route, field) => {
- const url = envs.APP_URL + ':' + envs.APP_PORT;
- return (objList.map( (item) => {
-         item.dataValues.detail = url + route + item[field];
-         return item
-     })
- );
-} */
-/*
-    */
 
--- agro-mapache/routes/api.js
+++ agro-mapache/routes/api.js
@@ -5,10 +5,10 @@ const apiControllers = require('../controllers/apiControllers')
 
 router.get('/products', apiControllers.productAll)
 router.get('/product/:product_id',apiControllers.productDetail) 
-router.get('/user/:product_id/image', apiControllers.productImage)
+
 router.get('/users/', apiControllers.usersAll)
 router.get('/user/:customer_id', apiControllers.userDetail)
-router.get('/user/:customer_id/image', apiControllers.userImage)
+
 
 
 module.exports = router;
